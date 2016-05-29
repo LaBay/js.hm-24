65efa6c0ae18eb5465e9bbed679c8170bea6bc25
“LaBay” <labay@i.ua>
2016-05-30 01:22:38 +0300

    .

---
+++ js/script.js
@@ -0,0 +1,25 @@
+﻿requirejs.config({
+	paths: {
+	
+		'tmpl': 'tmpl',
+	},
+	shim:{
+
+		'tmpl': {
+			exports: 'tmpl'
+		}
+	}
+});
+
+require(
+	[
+	'jquery','tmpl','model','view','controller'
+	],
+function(jquery, tmpl, Model, View, Controller){
+	$(function (){
+	var firstToDoList =['eat', 'drink', 'sleep'];
+	var model = new Model(firstToDoList);
+	var view = new View(model);
+	var controller = new Controller(model, view);
+	})	
+});
